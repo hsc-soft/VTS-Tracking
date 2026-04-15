18687d29504a08cfb0a559bbf613b305f2b92f04
hsc-soft <hsc.soft@gmail.com>
2026-04-15 15:34:08 +0530

    tocken update

--- src/redis.js
+++ src/redis.js
@@ -2,7 +2,7 @@ const { Redis } = require('@upstash/redis');
 
 const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL || 'https://logical-calf-68263.upstash.io',
-  token: process.env.UPSTASH_REDIS_REST_TOKEN || 'gQAAAAAAAAQqnAAIncDI1MmE3MTJiMWJlMjk0MGQ5YjBmMmQzNDAzMTAyMWYxM3AyNjgyNjM',
+  token: process.env.UPSTASH_REDIS_REST_TOKEN || 'gQAAAAAAAQqnAAIncDI1MmE3MTJiMWJlMjk0MGQ5YjBmMmQzNDAzMTAyMWYxM3AyNjgyNjM',
 });
 
 // Test Redis connection

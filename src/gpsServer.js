7beb84b8e106a1f8c7098e7b31308922d58ce48e
hsc-soft <hsc.soft@gmail.com>
2026-06-20 08:19:11 +0530

    soket closed logs

--- src/gpsServer.js
+++ src/gpsServer.js
@@ -1232,11 +1232,11 @@ function startGPSServer(port) {
     const connectedAt = Date.now();
     socket.on("close", (hadError) => {
 
-      // console.log(`❌ CLOSE IMEI:${imei}hadError:${hadError}read:${socket.bytesRead}write:${socket.bytesWritten}`
-      //  );
-
+      console.log(`❌ CLOSE IMEI:${imei}hadError:${hadError}read:${socket.bytesRead}write:${socket.bytesWritten}`
+      );
+      console.log("Connected for", (Date.now() - connectedAt) / 1000, "seconds");
       if (gt06Mode) {
-        console.log("Connected for", (Date.now() - connectedAt) / 1000, "seconds");
+
         console.log("========== SOCKET CLOSED ==========");
         console.log({
           imei,
